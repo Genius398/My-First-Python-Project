4dc77b6429581f49c8efc0b0bae6cca8031bb05d
Anantjit <anantjitchander@gmail.com>
2021-04-15 21:07:33 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,11 @@
+name = input("what is your name?")
+
+print("hello "+name+"!")
+
+
+scratch = input("do you like scratch? yes of no")
+
+if scratch == "no":
+    print("ok...")
+elif scratch == "yes":
+    print("Great!I alo love it")
